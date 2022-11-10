be51b640b0349f61e45223cd7fe5253b63ba78eb
Aman-dev1504 <Amanmalviya786an@gmail.com>
2022-11-11 00:31:56 +0530

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
-
+import Graph from './components/graph';
+import Form from './components/form';
 function App() {
   return (
     <div className="App">
-    <h1>Expense Tracker</h1>
+      <div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-900">
+    <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-white rounded">Expense Tracker💸</h1>
+    {/*Grid coloums*/}
+    <div className="grid md:grid-cols-2 gap-4">
+      {/*Chart*/}
+      <Graph></Graph>
+      {/*Form*/}
+      <Form></Form>
+    </div>
+      </div>
     </div>
   );
 }
---
+++ client/src/components/graph.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import {Doughnut} from 'react-chartjs-2';
+import { Chart ,ArcElement} from 'chart.js';
+import Labels from './labels.js'
+Chart.register(ArcElement)
+  const config={
+    data:{
+        datasets: [{
+            data: [300, 50, 100],
+            backgroundColor: [
+              'rgb(255, 99, 132)',
+              'rgb(54, 162, 235)',
+              'rgb(255, 205, 86)'
+            ],
+            hoverOffset: 4,
+            borderRadius:10,
+            spacing:8
+          }]
+    },
+    options:{
+        cutout:100
+    }
+  }
+export default function graph() {
+  return (
+    <div className="flex justify-content max-w-xs mx-auto">
+        <div className="item">
+            <div className="chart relative">
+            <Doughnut {...config}></Doughnut>
+            <h3 className="mb-4 front-bold title">Total
+            <span className='block text-3xl text-emerald-400'>${0}</span>
+            </h3>
+            </div>
+            <div className="flex flex-col py-10 gap-4">
+                {/*Labels*/}
+            <Labels></Labels>
+            </div>
+        </div>
+      
+    </div>
+  )
+}

8a1086071f50e03cefaeae2cf37dd2f9bf729456
Aman-dev1504 <Amanmalviya786an@gmail.com>
2023-02-06 18:11:31 +0530

    transaction api

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -13,11 +13,53 @@ async function create_Categories(req,res){
 }
 
 //Get req :: https://localhost:8080/api/categories
-async function get_categories(req,res){
+async function get_Categories(req,res){
     let data = await model.Categories.find({})
+    let filter=await data.map(v=>Object.assign({},{type:v.type,color:v.color}));
+    return res.json(filter);
+}
+
+//post req :: https://localhost:8080/api/transaction
+async function create_Transaction(req,res){
+    if(!req.body) return res.status(400).json("Data not provided!");
+let{name,type,amount}=req.body;
+
+const create= await new model.Transaction(
+    {
+        name,
+        type,
+        amount,
+        date: new Date()
+    }
+);
+create.save(function(err){
+    if(!err) return res.json(create);
+    return res.status(400).json({message:`Error while creating Transaction ${err}`});
+})
+
+}
+
+//get req :: https://localhost:8080/api/transaction
+async function get_Transaction(req,res){
+    let data=await model.Transaction.find({});
     return res.json(data);
 }
+
+//del eq :: https://localhost:8080/api/transaction
+
+async function del_Transaction(req,res){
+    if(!req.body) res.status(400).json({message:"Req Body not found!"});
+    await model.Transaction.deleteOne(req.body,function(err){
+        if(!err) res.json("Record Deleted..!");
+    }).clone().catch(function(err){
+        res.json("Error while Deleting Transaction")
+    });
+}
+
 module.exports={
     create_Categories,
-    get_categories
+    get_Categories,
+    create_Transaction,
+    get_Transaction,
+    del_Transaction
 }
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,6 +1,12 @@
 const routes=require ('express').Router();
 const controller =require('../controller/controller')
+
 routes.route('/api/categories')
 .post(controller.create_Categories)
-.get(controller.get_categories)
+.get(controller.get_Categories)
+
+routes.route('/api/transaction')
+.post(controller.create_Transaction)
+.get(controller.get_Transaction)
+.delete(controller.del_Transaction)
 module.exports=routes;
